792a410530ffde9cd1fe87c667e123d1e86ed555
Yevheniia2608 <Olinovskayaevgenia@gmail.com>
2026-07-01 20:01:56 +0300

    Updated context for AI, and made some changes in styling

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generateSaying(event) {
  let instructionsInput = document.querySelector("#user-instructions");
  let apiKey = "f11b87o82t2bbbea090de2e7338b0747";
  let context =
-   "You are very smart and well-read, and you know many famous sayings. Please generate a short famous saying in basic HTML and separate each line with a <br />. Do Not display html in your answer. Make sure to follow the user instructions. Please include author of the saying. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the saying and NOT at the beginning ";
+   " You are very smart and well-read, and you know many famous sayings. Please generate a short famous saying in basic HTML and separate each line with a <br />. Return only HTML in the format <p>Famous saying</p>. Make sure to follow the user instructions. Please include author of the saying. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the saying and NOT at the beginning "
  let prompt = `User instructions: Generate a famous saying about ${instructionsInput.value}`;
  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
  let sayingElement = document.querySelector("#saying");
  sayingElement.classList.remove("hidden");
- sayingElement.innerHTML = `<div class="generating">⏳ Generating a famous saying about ${instructionsInput.value}</div>`;
+ sayingElement.innerHTML = `<div class="generating">⏳ Generating a famous saying about ${instructionsInput.value}...</div>`;
 
  axios.get(apiURL).then(displaySaying);
 }
